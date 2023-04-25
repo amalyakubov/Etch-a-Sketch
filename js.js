520ba3f2fdf0e8c900b67db940bfa6de6188f0a4
amalyakubov <129682707+amalyakubov@users.noreply.github.com>
2023-04-25 16:14:05 +0200

    Fix the event listener with the use of Array from() funciton
    Add the Array.from() to the document.getByClassName() in order to fix
    the function that loops of the forEach and adds the addEventListner()
    to the div elements inside the container.

--- js.js
+++ js.js
@@ -2,8 +2,19 @@ const container = document.getElementById('container');
 function createGrid() {
     for (let  i = 0; i < 256; i++) {
         let divs = document.createElement('div');
+        divs.classList.add('pixel');
         container.appendChild(divs);
     }
 }
 
 createGrid();
+container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+container.style.gridTemplateRows = `repeat(16, 1fr)`;
+
+let pixels = document.querySelectorAll('pixel');
+
+Array.from(document.getElementsByClassName('pixel')).forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = 'red';
+    })
+})
