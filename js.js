06dd74e060e2035773fbd780a8cc8c7d676d5250
amalyakubov <129682707+amalyakubov@users.noreply.github.com>
2023-04-25 16:54:30 +0200

    Add the prompt message, asking user for the size
    Initialize the variable 'size' with the value of prompt converted to a
    number. If the 'size' is smaller than 0 or larger than 100, set the
    'size' to 0 and display a message. This is needed, so that the user
    cannot generate a grid large enough, for the browser to freeze or crash.
    
    Edit the gridTemplateColumns and grid TemplateRows to use the ${size}
    as their first argument. This is essential for the js code to run
    properly, as it sets the number of rows and columnds to be equal the
    value of the 'size' variable.
    
    Edit the style.css to make all children of the container to not shrink.
    Set the height and width of the container to be equal 960px, because it
    is needed for the container to remain the same size, even with different
    inputs from the user.

--- js.js
+++ js.js
@@ -1,15 +1,20 @@
 const container = document.getElementById('container');
 function createGrid() {
-    for (let  i = 0; i < 256; i++) {
+    let size = +prompt('Enter the size');
+    if (size < 0 || size > 100) {
+        alert('Invalid size.');
+        size = 0;
+    }
+    for (let  i = 0; i < size * size; i++) {
         let divs = document.createElement('div');
         divs.classList.add('pixel');
         container.appendChild(divs);
     }
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 }
 
 createGrid();
-container.style.gridTemplateColumns = `repeat(16, 1fr)`;
-container.style.gridTemplateRows = `repeat(16, 1fr)`;
 
 let pixels = document.querySelectorAll('pixel');
 
